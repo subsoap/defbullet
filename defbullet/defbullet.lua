449ae48765382b8cc5ab9176a607b51f89e4ca09
Brian Kramer <subsoap@gmail.com>
2018-01-26 03:00:37 -0800

    Progress with built in detection and raycasts

--- defbullet/defbullet.lua
+++ defbullet/defbullet.lua
@@ -1,4 +1,15 @@
--- Put functions in this file to use them in several other scripts.
--- To get access to the functions, you need to put:
--- require "my_directory.my_file"
--- in any script using the functions.
+
+local M = {}
+
+M.bullets = {}
+
+function M.update(dt)
+end
+
+function M.create_bullet(x, y, velocity, direction, attributes, sprite_component, sprite, delay)
+	
+end
+
+
+
+return M
--- example/bullet.go
+++ example/bullet.go
@@ -1,9 +1,39 @@
+components {
+  id: "fader"
+  component: "/example/fader.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "bullet"
+  component: "/example/bullet.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/example/gfx/bullets.atlas\"\n"
   "default_animation: \"bullet_kunai\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
+  "material: \"/example/materials/sprite_color_add.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
   position {
